9d9f59df8af88ed41b63622df2759d3fd254f4d4
pxn626 <790864000@qq.com>
2021-01-15 13:15:58 +0800

    Functional_test input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,30 +1,45 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		# open index  
-		self.browser.get('http://localhost:8000')
-		
-		self.assertIn('To-Do', self.browser.title)
-		self.fail("Finish the test!")
-		
-		# in text input "Buy Fish"
-		# ENTER
-		# IN text output "1: Buy Fish"
-		# in text input "Buy rice"
-		# ENTER
-		# in text output "2: Buy rice"
-		
-		# output the only URL
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # open index  
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        # in text input "Buy Fish"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'  
+        )
+        inputbox.send_keys('Buy Fish')
+        # ENTER
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        # IN text output "1: Buy Fish"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy Fish' for row in rows)
+        )
+        # in text input "Buy rice"
+        # ENTER
+        # in text output "2: Buy rice"
+        self.fail('Finish the test')
+        # output the only URL
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
