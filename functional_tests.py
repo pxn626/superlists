9b5fb861bd0bacb0ef0a4d1505954285221e3de8
pxn626 <790864000@qq.com>
2021-01-15 10:35:50 +0800

    使用了unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# open index  
+		self.browser.get('http://localhost:8000')
+		
+		self.assertIn('To-Do', self.browser.title)
+		self.fail("Finish the test!")
+		
+		# in text input "Buy Fish"
+		# ENTER
+		# IN text output "1: Buy Fish"
+		# in text input "Buy rice"
+		# ENTER
+		# in text output "2: Buy rice"
+		
+		# output the only URL
+
+
+if __name__ == '__main__':
+	unittest.main()
