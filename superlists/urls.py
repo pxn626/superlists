5914426a5c002d6c3aec0d65bde246c2da795bbc
pxn626 <790864000@qq.com>
2021-01-16 13:54:15 +0800

    new URL,view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/tolitw/', views.view_list, name='view_list'),
 ]
